d228f1c855c7a0cbea818832ff8fa0a2b92803fa
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-03-13 20:53:39 +0000

    Add getBeachList & getFoodList functions and content

--- src/wishList.js
+++ src/wishList.js
@@ -1,5 +1,16 @@
 'use strict';
 
 function WishList () {
-
+  this.DEFAULT_BEACH_LIST = ["Ball", "Bat", "Bodyboard", "Bucket"];
+  this.DEFAULT_FOOD_LIST = ["Salami", "Prosciutto", "Ham", "Steak"];
+  this._beachList = this.DEFAULT_BEACH_LIST;
+  this._foodList = this.DEFAULT_FOOD_LIST;
 }
+
+WishList.prototype.getBeachList = function() {
+  return this._beachList;
+};
+
+WishList.prototype.getFoodList = function() {
+  return this._foodList;
+};
