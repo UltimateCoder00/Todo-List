96036f76c6b8496b503a314b2ebda4adb7891f38
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-03-13 21:27:21 +0000

    Add remove items from wish lists functions

--- src/wishList.js
+++ src/wishList.js
@@ -23,6 +23,16 @@ WishList.prototype.addToFoodList = function(item) {
   this._foodList.push(item);
 };
 
+WishList.prototype.removeFromBeachList = function(item) {
+  var index = this._beachList.indexOf(item);
+  this._beachList.splice(index, 1);
+};
+
+WishList.prototype.removeFromFoodList = function(item) {
+  var index = this._foodList.indexOf(item);
+  this._foodList.splice(index, 1);
+};
+
 WishList.prototype.clearBeachList = function() {
   this._beachList = [];
 };
