0ef124535bcd8b9d7c3a0cb8b8031a3f5718b020
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-03-13 20:54:49 +0000

    Add wish list default tests

--- spec/wishListSpec.js
+++ spec/wishListSpec.js
@@ -2,5 +2,17 @@ console.log('WishList Spec')
 
 'use strict';
 describe('WishList', function(){
+  var wishList;
 
+  beforeEach(function() {
+    wishList = new WishList();
+  });
+
+  it('Wish list has default beach list', function() {
+    expect(wishList.getBeachList()).toEqual(["Ball", "Bat", "Bodyboard", "Bucket"]);
+  });
+
+  it('Wish list has default food list', function() {
+    expect(wishList.getFoodList()).toEqual(["Salami", "Prosciutto", "Ham", "Steak"]);
+  });
 });
